6698fed71dd322e0519b63f6d4acf992a4d56113
Dit Dejphachon <Ditdejphachon@gmail.com>
2022-05-20 15:21:56 +0700

    match

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
-fn main() {
-    let string = "Hello, World!";
+use rand::Rng;
 
-    println!("{}", string);
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("Guess the number !");
+    println!("Please input your guess");
+
+    loop {
+        let mut guess = String::new();
+        std::io::stdin()
+            .read_line(&mut guess)
+            .expect("failed to read line ");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("you guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            std::cmp::Ordering::Less => println!("Too small!"),
+            std::cmp::Ordering::Greater => println!("Too big!"),
+            std::cmp::Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
 }
