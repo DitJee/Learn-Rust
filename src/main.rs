426dd1156ff5d7ea3f5c2bb56bdd99a8b0a0738a
Dit Dejphachon <Ditdejphachon@gmail.com>
2022-05-20 17:13:19 +0700

    ownership#1

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,25 @@
-use std::io;
-
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    let s = String::from("hello"); // s comes into scope
 
-    println!("Please enter an array index.");
+    takes_ownership(s); // s's value moves into the function...
+                        // ... and so is no longer valid here
+    println!("{}", s);
+    let x = 5; // x comes into scope
 
-    let mut index = String::new();
+    makes_copy(x); // x would move into the function,
+                   // but i32 is Copy, so it's okay to still
+                   // use x afterward
+    println!("{}", x);
+} // Here, x goes out of scope, then s. But because s's value was moved, nothing
+  // special happens.
 
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
+fn takes_ownership(some_string: String) {
+    // some_string comes into scope
+    println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
 
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
-
-    let element = a[index];
-
-    println!(
-        "The value of the element at index {} is: {}",
-        index, element
-    );
-}
+fn makes_copy(some_integer: i32) {
+    // some_integer comes into scope
+    println!("{}", some_integer);
+} // Here, some_integer goes out of scope. Nothing special happens.
